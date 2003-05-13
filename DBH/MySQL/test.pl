2a86e34ee31a7bc7a75d9a81ce43bbff3d678eaa
jaredy <>
2003-05-13 13:05:05 +0000

    added test driver

---
+++ DBH/MySQL/test.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -W
+# $Id$
+
+use strict;
+use DBH::MySQL;
+
+my $dbh = DBH::MySQL->new(host=>"12.226.98.118", database=>"test");
+
+$dbh->query("CREATE TABLE foo (a INT)", DB_NULL);
+$dbh->query("INSERT INTO foo (5)", DB_NULL);
+
+print "This should be 5: ", $dbh->query("SELECT FROM foo", DB_COL), "\n";
+
+exit 0;
