aaa2c16a81bb9f318e7ad89230c9ac9c5e58f1ef
jaredy <>
2003-05-13 13:54:35 +0000

    import DBH consts, override and propagate new(), specifying mysql dbi driver and filling default port

--- DBH/MySQL/0.1.pm
+++ DBH/MySQL/0.1.pm
@@ -1,13 +1,22 @@
 package DBH::MySQL;
 # $Id$
 
-use DBH;
+use DBH qw(:all);
 use strict;
 
 our $VERSION = 0.1;
 our @ISA = qw(DBH);
 our @EXPORT_OK = @DBH::EXPORT_OK;
 our %EXPORT_TAGS = %DBH::EXPORT_TAGS;
+
+sub new
+{
+	my ($class, %prefs) = @_;
+	$prefs{driver} = "mysql";
+	# Set default port if unspecified
+	$prefs{port} = 3306 if !exists $prefs{port} && exists $prefs{host};
+	my $this = $class->SUPER::new(%prefs);
+}
 
 sub last_insert_id
 {
