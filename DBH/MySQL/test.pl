00fb87ca76e399b5418f93b5b216692d48933092
jaredy <>
2003-05-13 13:53:26 +0000

    added sql to run test multiple times (state-safe), select -> select *, formalized INSERT query

--- DBH/MySQL/test.pl
+++ DBH/MySQL/test.pl
@@ -6,11 +6,12 @@ use WASP;
 use DBH::MySQL qw(:all);
 
 my $w = WASP->new();
-my $dbh = DBH::MySQL->new(wasp=>$w, host=>"12.226.98.118", database=>"test");
+my $dbh = DBH::MySQL->new(wasp=>$w, database=>"test");
 
+$dbh->query("DROP TABLE IF EXISTS foo", DB_NULL);
 $dbh->query("CREATE TABLE foo (a INT)", DB_NULL);
-$dbh->query("INSERT INTO foo (5)", DB_NULL);
+$dbh->query("INSERT INTO foo (a) VALUES (5)", DB_NULL);
 
-print "This should be 5: ", $dbh->query("SELECT FROM foo", DB_COL), "\n";
+print "This should be 5: ", $dbh->query("SELECT * FROM foo", DB_COL), "\n";
 
 exit 0;
